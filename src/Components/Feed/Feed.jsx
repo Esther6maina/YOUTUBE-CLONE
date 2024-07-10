52618695e2fbe6bb073409dabdff03a565a7450b
Esther Maina <estanyambura99@gmail.com>
2024-07-10 14:57:28 +0300

    initial commit

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -14,12 +14,12 @@ const Feed = () => {
   return (
 
     <div className="feed">    
-      <div className='card'>
+      <link to={Video/20/4521} className='card'>
         <img src={thumbnail1} alt=""/>
         <h2>Best channel to learn coding that help you to be a web developer</h2>
         <h3>Greatstack</h3>
         <p>15k views &bull; 2days ago</p>
-      </div>
+      </link>
       <div className='card'>
         <img src={thumbnail2} alt=""/>
         <h2>Best channel to learn coding that help you to be a web developer</h2>
