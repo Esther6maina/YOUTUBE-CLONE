10e8d87bdc4a33fd75592842bff4fa924f4377b9
Esther Maina <estanyambura99@gmail.com>
2024-07-12 20:53:24 +0300

    initial commit

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -14,7 +14,43 @@ const PlayVideo = () => {
   return (
     <div className='play-video'>
       <video src={video} controls autoPlay muted></video>
+      <h3>Best Youtube Channel To Learn Web Development</h3>
+      <div className='play-video-info'>
+      <p>1525 views &bull; 2 days ago</p>
+      <div>
+        <span><img src={like} alt=""/>125</span>
+        <span><img src={dislike} alt=""/>2</span>
+        <span><img src={share} alt=""/>share</span>
+        <span><img src={save} alt=""/>save</span>
+      </div>
+      </div>
+      <hr />
+      <div className="publisher">
+        <img src={jack} alt=""/>
+        <div>
+          <p>Greatstack</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className='vid-description'>
+        <p>Channel that makes learning easy</p>
+        <p>Subscribe Greatstack to watch more tutorials on web development</p>
+        <hr />
+        <h4>130 comments</h4>
+        <div className="comment">
+          <img src={user_profile} alt=""></img>
+          <div>
+            <h3>Jack Nicholson<span>1 day ago</span></h3>
+            <p>A global computer network providing a variety of information and commuincation of interconnected networks using standardizedcommunication protocols.</p>
+            <div className='comment-action'>
 
+            </div>
+          </div>
+        
+                       
+        </div>
+      </div>
     </div>
   )
 }
